ec091395bb707254180f6e2cba1a388fb7157154
gingaminga <gingahae@gmail.com>
2023-03-09 23:54:23 +0900

    💡 Chore: 로그 추가

--- src/index.ts
+++ src/index.ts
@@ -14,9 +14,11 @@ interface IActivityData {
 // 아래는 사용법을 보여주는 샘플 로직입니다.
 (async () => {
   try {
+    console.log('***** 어떤 활동을 해볼까? *****');
+
     const { data } = await Bored.getInstance().get<undefined, IActivityData>(url.BORED.PATH.ACTIVITY);
 
-    console.log(data);
+    console.log(`이게 좋겠네요 :) '${data.activity}'`);
   } catch (error) {
     console.error(error);
   }
